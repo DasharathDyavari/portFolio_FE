6fce8326c5062fb28968b7b2c0f390d8858620a2
Dasharath Dyavari <dyavaridasharath82@gmail.com>
2023-08-15 22:46:06 +0530

    feat(about): design about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import about_image from '../images/about.jpg';
 
 function About() {
   return (
     <div className='about'>
-        about
+        <div className="about-image">
+            <div className='about-img-background'></div>
+            <img className='about-img' src={about_image} alt="about" />
+        </div>
+        <div className='about-text-content'>
+            <h2 className='about-text-header'>about <span>me</span></h2>
+            <h4 className='about-text-subheading'>Web Developer</h4>
+            <p className='about-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit ducimus incidunt maxime quaerat quam facilis quidem a mollitia at sapiente!</p>
+        </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Navbar() {
   return (
@@ -17,14 +12,6 @@ function Navbar() {
             <li className='navbar-item'><a href="">projects</a></li>
             <li className='navbar-item'><a href="">skills</a></li>
             <li className='navbar-item'><a href="">contacts</a></li>
-        </ul>
-        <ul className='navbar-icons'>
-            <li className='navbar-icon'><a href="https://www.facebook.com/dasharath.dasharath.1694/" target='_blank'><FacebookIcon color='action'/></a></li>
-            <li className='navbar-icon'><a href="https://www.instagram.com/dasharath_116/" target='_blank'><InstagramIcon color='action'/></a></li>
-            <li className='navbar-icon'><a href="https://github.com/Dasharath9920" target='_blank'><GitHubIcon color='action'/></a></li>
-            <li className='navbar-icon'><a href="https://www.linkedin.com/in/dyavari-dasharath-b767021b3/" target='_blank'><LinkedInIcon color='action'/></a></li>
-            <li className='navbar-icon'><a href="https://twitter.com/Dashara38665028" target='_blank'><TwitterIcon color='action'/></a></li>
-
         </ul>
     </div>
   )
