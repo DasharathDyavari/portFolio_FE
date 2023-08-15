6dd71f6e4cfb5e16051b043ef90d131234d15692
Dasharath Dyavari <dyavaridasharath82@gmail.com>
2023-08-15 20:56:25 +0530

    feat(home): design hero part in home section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import profile_pic from '../images/hero.png';
 
 function Hero() {
   return (
     <div className='hero'>
-        
+        <div className="text-content">
+            <h3 className='hero-subheading1'>Hello, This is</h3>
+            <h2 className='hero-heading'>Dasharath</h2>
+            <h3 className='hero-subheading2'>
+                <p>I'm a</p>
+                <ul className='dynamic-text'>
+                    <li><span>Developer</span></li>
+                    <li><span>Rider</span></li>
+                    <li><span>Editor</span></li>
+                    <li><span>Youtuber</span></li>
+                </ul>
+
+            </h3>
+            <button className='download-btn'>Download Resume</button>
+        </div>
+        <div className='hero-image'>
+            <img src={profile_pic} alt="image" />
+        </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Navbar() {
   return (
@@ -7,17 +12,19 @@ function Navbar() {
             <h2>Logo</h2>
         </div>
         <ul className='navbar-items'>
-            <li className='navbar-item'>home</li>
-            <li className='navbar-item'>about me</li>
-            <li className='navbar-item'>projects</li>
-            <li className='navbar-item'>skills</li>
-            <li className='navbar-item'>contacts</li>
+            <li className='navbar-item'><a href="">home</a></li>
+            <li className='navbar-item'><a href="">about me</a></li>
+            <li className='navbar-item'><a href="">projects</a></li>
+            <li className='navbar-item'><a href="">skills</a></li>
+            <li className='navbar-item'><a href="">contacts</a></li>
         </ul>
         <ul className='navbar-icons'>
-            <li className='navbar-icon'><a href="https://www.facebook.com/dasharath.dasharath.1694/" target='_blank'><img src="https://img.icons8.com/ios/50/000000/facebook--v1.png"/></a></li>
-            <li className='navbar-icon'><a href="https://www.instagram.com/dasharath_116/" target='_blank'><img src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png"/></a></li>
-            <li className='navbar-icon'><a href="https://github.com/Dasharath9920" target='_blank'><img src="https://img.icons8.com/ios/50/000000/github--v1.png"/></a></li>
-            <li className='navbar-icon'><a href="https://www.linkedin.com/in/dyavari-dasharath-b767021b3/" target='_blank'><img src="https://img.icons8.com/ios/50/000000/linkedin.png"/></a></li>
+            <li className='navbar-icon'><a href="https://www.facebook.com/dasharath.dasharath.1694/" target='_blank'><FacebookIcon color='action'/></a></li>
+            <li className='navbar-icon'><a href="https://www.instagram.com/dasharath_116/" target='_blank'><InstagramIcon color='action'/></a></li>
+            <li className='navbar-icon'><a href="https://github.com/Dasharath9920" target='_blank'><GitHubIcon color='action'/></a></li>
+            <li className='navbar-icon'><a href="https://www.linkedin.com/in/dyavari-dasharath-b767021b3/" target='_blank'><LinkedInIcon color='action'/></a></li>
+            <li className='navbar-icon'><a href="https://twitter.com/Dashara38665028" target='_blank'><TwitterIcon color='action'/></a></li>
+
         </ul>
     </div>
   )
